37660732f34b6252fd668a52083cedd988dd776e
ppriyanka <ppriyanka@umass.edu>
2026-03-12 16:08:11 -0400

    add iterators_generators log file

--- python/iterators_generators/create_log_file.py
+++ python/iterators_generators/create_log_file.py
@@ -2,7 +2,8 @@ import random
 
 levels = ["INFO", "DEBUG", "WARNING", "ERROR"]
 
-with open("server.log", "w")  as f:
-    for i in range(100):
+with open("server.log", "w") as f:
+    for i in range(10000):
         level = random.choice(levels)
         f.write(f"{level}: message number {i}\n")
+
